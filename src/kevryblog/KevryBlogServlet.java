3ff05fbabe91b743b774d3f44c3b7f407f960a62
Krosen11 <larrybarnson@gmail.com>
2016-02-09 13:28:03 -0600

    Some changes

--- src/kevryblog/KevryBlogServlet.java
+++ src/kevryblog/KevryBlogServlet.java
@@ -1,13 +1,26 @@
 package kevryblog;
 
 import java.io.IOException;
+
 import javax.servlet.http.*;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 @SuppressWarnings("serial")
 public class KevryBlogServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		UserService userService = UserServiceFactory.getUserService();
+      User user = userService.getCurrentUser();
+      if (user != null) {
+      	//This is when the user is logged in
+         resp.setContentType("text/plain");
+         resp.getWriter().println("Hello, " + user.getNickname());
+      } else {
+      	//User is not logged in
+         resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
+      }
 	}
 }
